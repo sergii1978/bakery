7a5ff1b0e253febe1e66a00a7d1f2784668e4ed9
Sergii Storcheus <44782756+sergii1978@users.noreply.github.com>
2024-04-21 01:49:28 +0300

    correct slider
    correct slider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-// slick slider
-$('.multiple-items').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+  },
+  navigation: {
+    nextEl: '.carousel-button.next',
+    prevEl: '.carousel-button.prev',
+  },
 });
   
   
